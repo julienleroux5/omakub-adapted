ceb0e58276aff7bea367fa5b2c5ef761e9964eb2
julienleroux5 <julien.leroux5@gmail.com>
2026-02-23 17:47:55 +0100

    allowing another branch than master (custom branch)

--- boot.sh
+++ boot.sh
@@ -20,11 +20,9 @@ sudo apt-get install -y git >/dev/null
 echo "Cloning Omakub..."
 rm -rf ~/.local/share/omakub
 git clone https://github.com/julienleroux5/omakub-adapted.git ~/.local/share/omakub >/dev/null
-if [[ $OMAKUB_REF != "master" ]]; then
-	cd ~/.local/share/omakub
-	git fetch origin "${OMAKUB_REF:-stable}" && git checkout "${OMAKUB_REF:-stable}"
-	cd -
-fi
+cd ~/.local/share/omakub
+git fetch origin feature/adapted-version && git checkout feature/adapted-version
+cd -
 
 echo "Installation starting..."
 source ~/.local/share/omakub/install.sh
