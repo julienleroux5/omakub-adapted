2dcc5baccdfea0bb2d447bc93c4f850242a2decf
julienleroux5 <julien.leroux5@gmail.com>
2026-02-23 18:19:28 +0100

    fixed noble cran repo for R

--- install/terminal/select-dev-language.sh
+++ install/terminal/select-dev-language.sh
@@ -14,7 +14,7 @@ if [[ -n "$languages" ]]; then
     R)
       sudo apt -y install --no-install-recommends software-properties-common dirmngr
       wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
-      sudo add-apt-repository -y "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+      sudo add-apt-repository -y "deb https://cloud.r-project.org/bin/linux/ubuntu noble-cran40/"
       sudo apt -y install --no-install-recommends R-base
       ;;
     Node.js)
