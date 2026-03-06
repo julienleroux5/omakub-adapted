87f17bf1c7f3f0f583b82171066579bd4ad25eef
julienleroux5 <julien.leroux5@gmail.com>
2026-03-06 22:13:29 +0100

    add ohmyposh installation

--- install/terminal/apps-terminal.sh
+++ install/terminal/apps-terminal.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 sudo apt install -y fzf zsh ripgrep bat eza lsd zoxide plocate apache2-utils fd-find
+
+if ! command -v oh-my-posh &> /dev/null; then
+  curl -s https://ohmyposh.dev/install.sh | bash -s
+fi
