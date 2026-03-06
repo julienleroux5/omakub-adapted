a906a67fe8c5be691d85ed575e936e6cbb6aa6db
julienleroux5 <julien.leroux5@gmail.com>
2026-03-06 22:29:32 +0100

    install beach theme during installation

--- install/desktop/set-gnome-theme.sh
+++ install/desktop/set-gnome-theme.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 source ~/.local/share/omakub/themes/beach/gnome.sh
+
+# Apply Beach custom GTK theme assets (window controls, GTK4 overrides, etc.)
+if [[ -f ~/.local/share/omakub/themes/beach/install.sh ]]; then
+  bash ~/.local/share/omakub/themes/beach/install.sh
+fi
+
 source ~/.local/share/omakub/themes/beach/tophat.sh
